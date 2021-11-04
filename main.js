68f1311ac5cfd60b556e76df4d21d9d238c7e14e
Lucas Pereira <lucasmdpereira@gmail.com>
2021-11-04 16:51:58 -0300

    Aula 04 – Aprofundando no JavaScript

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ for (const link of links) {
   })
 }
 
+/* back-to-top */
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
+
 //swipper
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
@@ -40,6 +50,7 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
   { interval: 100 }
 )
