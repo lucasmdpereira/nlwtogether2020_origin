057dfd16839fbfe3c16f6c5a4d6808d6c8f38ae5
Lucas Pereira <lucasmdpereira@gmail.com>
2021-11-09 16:56:09 -0300

    Aula 05 – Finalizando o projeto

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ for (const link of links) {
 
 /* back-to-top */
 const backToTopButton = document.querySelector('.back-to-top')
+
 window.addEventListener('scroll', function () {
+  activateMenuAtCurrentSection()
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -34,7 +36,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 //scrollreveal
@@ -54,3 +62,29 @@ scrollReveal.reveal(
   footer .brand, footer .social`,
   { interval: 100 }
 )
+
+/* Menu ativo conforme a seção visível na página*/
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
